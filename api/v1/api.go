ef6f1415c504b0cf874564955fd08179d3d1c6f5
dan <jianglin1008@gmail.com>
2020-08-21 16:15:12 +0800

    v1

--- api/v1/api.go
+++ api/v1/api.go
@@ -6,14 +6,16 @@ import (
 )
 
 type Api struct {
-	Srv    *app.Server
-	router *gin.Engine
+	Srv     *app.Server
+	router  gin.IRoutes
+	Version string
 }
 
 func Init(srv *app.Server, router *gin.Engine) *Api {
 	api := Api{
-		Srv:    srv,
-		router: router,
+		Srv:     srv,
+		router:  router.Group("/v1"),
+		Version: "v1",
 	}
 	api.InitUser()
 	return &api
